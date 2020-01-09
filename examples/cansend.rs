fdd79cc2996e9026bf0eb997de5e499df6faa31c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2020-01-10 00:26:50 +0900

    add len check

--- examples/cansend.rs
+++ examples/cansend.rs
@@ -5,7 +5,7 @@ fn main() -> Result<()> {
     let socket = CanSocket::bind("vcan0")?;
     socket.set_can_fd_frames(true)?;
 
-    let frame = CanFdFrame::new(22, &[0xDE, 0xAD, 0xBE, 0xEF, 0x01, 0x23, 0x45, 0x67]);
+    let frame = CanFdFrame::new(42, &[0xDE, 0xAD, 0xBE, 0xEF, 0x01, 0x23, 0x45, 0x67])?;
     socket.write_frame(&frame)?;
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod linux_can;
 
 use std::ffi::CString;
-use std::io::{Error, Result};
+use std::io::{Error, ErrorKind, Result};
 use std::mem;
 use std::os::raw::c_int;
 use std::os::unix::io::{AsRawFd, RawFd};
@@ -93,11 +93,22 @@ impl AsRawFd for CanSocket {
 pub type CanFdFrame = linux_can::canfd_frame;
 
 impl CanFdFrame {
-    pub fn new(can_id: u32, data: &[u8]) -> Self {
+    pub fn new(can_id: u32, data: &[u8]) -> Result<Self> {
         let mut frame = unsafe { mem::MaybeUninit::<Self>::zeroed().assume_init() };
         frame.can_id = can_id;
+        let len = match data.len() {
+            len @ 0..=8 => len,
+            9..=12 => 12,
+            13..=16 => 16,
+            17..=20 => 20,
+            21..=24 => 24,
+            25..=32 => 32,
+            33..=48 => 48,
+            49..=64 => 64,
+            _ => return Err(ErrorKind::InvalidInput.into()),
+        };
         frame.data[..data.len()].copy_from_slice(data);
-        frame.len = data.len() as _;
-        frame
+        frame.len = len as _;
+        Ok(frame)
     }
 }
