0faf70ce17c3e8b81c0a7677d0ab0248be85c127
Toru Ogawa <Hakuyume@users.noreply.github.com>
2020-01-10 12:49:20 +0900

    add tokio support

---
+++ src/async_can_socket.rs
@@ -0,0 +1,70 @@
+use crate::{CanFdFrame, CanSocket};
+use futures::future::poll_fn;
+use futures::ready;
+use mio::event::Evented;
+use mio::unix::EventedFd;
+use mio::{PollOpt, Token};
+use std::io::{ErrorKind, Result};
+use std::os::unix::io::AsRawFd;
+use std::task::{Context, Poll};
+use tokio::io::PollEvented;
+
+pub struct AsyncCanSocket(PollEvented<CanSocket>);
+
+impl AsyncCanSocket {
+    pub fn new() -> Result<Self> {
+        Ok(Self(PollEvented::new(CanSocket::new()?)?))
+    }
+
+    pub fn bind<I>(&self, ifname: I) -> Result<()>
+    where
+        I: Into<Vec<u8>>,
+    {
+        self.0.get_ref().bind(ifname)
+    }
+
+    pub fn set_fd_frames(&self, enable: bool) -> Result<()> {
+        self.0.get_ref().set_fd_frames(enable)
+    }
+
+    pub async fn recv(&self) -> Result<CanFdFrame> {
+        poll_fn(|cx| self.poll_recv(cx)).await
+    }
+
+    fn poll_recv(&self, cx: &mut Context<'_>) -> Poll<Result<CanFdFrame>> {
+        ready!(self.0.poll_read_ready(cx, mio::Ready::readable()))?;
+        match self.0.get_ref().recv() {
+            Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                self.0.clear_read_ready(cx, mio::Ready::readable())?;
+                Poll::Pending
+            }
+            r => Poll::Ready(r),
+        }
+    }
+}
+
+impl Evented for CanSocket {
+    fn register(
+        &self,
+        poll: &mio::Poll,
+        token: Token,
+        interest: mio::Ready,
+        opts: PollOpt,
+    ) -> Result<()> {
+        EventedFd(&self.as_raw_fd()).register(poll, token, interest, opts)
+    }
+
+    fn reregister(
+        &self,
+        poll: &mio::Poll,
+        token: Token,
+        interest: mio::Ready,
+        opts: PollOpt,
+    ) -> Result<()> {
+        EventedFd(&self.as_raw_fd()).reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &mio::Poll) -> Result<()> {
+        EventedFd(&self.as_raw_fd()).deregister(poll)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+#[cfg(feature = "async_await")]
+mod async_can_socket;
 mod can_fd_frame;
 mod can_socket;
 mod linux_can;
 
+#[cfg(feature = "async_await")]
+pub use async_can_socket::AsyncCanSocket;
 pub use can_fd_frame::CanFdFrame;
 pub use can_socket::CanSocket;
