a58e7d83b3d27e0a770a403ed523c2b917520dd4
Toru Ogawa <Hakuyume@users.noreply.github.com>
2020-01-18 18:19:09 +0900

    add example in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! Rust binding for [SocketCAN](https://www.kernel.org/doc/Documentation/networking/can.txt)
+//!
+//! ## Example
+//!
+//! ```no_run
+//! use socketcan::{CanFrame, CanSocket, CanStandardFrame};
+//! use std::ffi::CString;
+//!
+//! let socket = CanSocket::bind(CString::new("vcan0")?)?;
+//! socket.set_recv_own_msgs(true)?;
+//!
+//! let frame = CanStandardFrame::new(0x42, &[0, 1, 2, 3, 4, 5, 6, 7]);
+//! socket.send(&CanFrame::Standard(frame))?;
+//!
+//! let frame = socket.recv()?;
+//! println!("{:?}", frame);
+//!
+//! # std::io::Result::Ok(())
+//! ```
+
 mod can_frame;
 mod can_socket;
 mod sys;
