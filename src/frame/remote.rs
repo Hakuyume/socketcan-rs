5eb5e96168ebb174298c45344001a77eca5c8cba
Toru Ogawa <Hakuyume@users.noreply.github.com>
2020-01-19 09:36:34 +0900

    support dlc in RemoteFrame

--- src/frame/remote.rs
+++ src/frame/remote.rs
@@ -9,8 +9,9 @@ pub struct RemoteFrame(pub(super) sys::can_frame);
 impl RemoteFrame {
     /// # Panics
     ///
-    /// Panics if `id` exceeds its limit.
-    pub fn new(id: Id) -> Self {
+    /// Panics if `id` exceeds its limit or `len` is greater than 8.
+    pub fn new(id: Id, len: usize) -> Self {
+        assert!(len <= sys::CAN_MAX_DLEN as _);
         let mut inner = MaybeUninit::<sys::can_frame>::zeroed();
         unsafe {
             (*inner.as_mut_ptr()).can_id = id.into_can_id();
@@ -21,12 +22,17 @@ impl RemoteFrame {
     pub fn id(&self) -> Id {
         Id::from_can_id(self.0.can_id)
     }
+
+    pub fn len(&self) -> usize {
+        self.0.can_dlc as _
+    }
 }
 
 impl fmt::Debug for RemoteFrame {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt.debug_struct("RemoteFrame")
             .field("id", &self.id())
+            .field("len", &self.len())
             .finish()
     }
 }
