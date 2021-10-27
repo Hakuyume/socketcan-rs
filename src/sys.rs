7572a5f702bc34a978fa79ba86ddda4d04ffb259
Marcel Hellwig <github@cookiesoft.de>
2021-10-27 11:22:39 +0200

    make changes to reflect can_frame struct change
    according to
    https://github.com/linux-can/can-utils/commit/c398e56afb9b60441232736e05fdfb5a525363ca
    the `can_dlc` field is deprected now and `len` should be used
    instead.
    
    But for older versions, we would like to keep it buildable, therefore a
    new feature flag is introduced: can-dlc-unaliased

--- src/sys.rs
+++ src/sys.rs
@@ -5,3 +5,27 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub unsafe fn can_frame_len(frame: *const can_frame) -> u8 {
+    #[cfg(feature = "can-dlc-unaliased")]
+    {
+        (*frame).can_dlc
+    }
+
+    #[cfg(not(feature = "can-dlc-unaliased"))]
+    {
+        (*frame).__bindgen_anon_1.len
+    }
+}
+
+pub unsafe fn can_frame_len_mut(frame: *mut can_frame) -> *mut u8 {
+    #[cfg(feature = "can-dlc-unaliased")]
+    {
+        &mut (*frame).can_dlc
+    }
+
+    #[cfg(not(feature = "can-dlc-unaliased"))]
+    {
+        &mut (*frame).__bindgen_anon_1.len
+    }
+}
