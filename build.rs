8de667ecf5f59057b126f7644bf555985bca0e0a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2021-10-28 23:31:11 +0900

    no comments

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen::Builder::default()
         .header("wrapper.h")
+        .generate_comments(false)
         .generate()
         .unwrap()
         .write_to_file(out_dir.join("bindings.rs"))
